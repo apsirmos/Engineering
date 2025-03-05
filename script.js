7b43dcd4a9d2a56677f1c168a1975779bc340661
apsirmos <96848158+apsirmos@users.noreply.github.com>
2025-03-05 10:37:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Κλείσιμο του Intro μετά από 3 δευτερόλεπτα
     setTimeout(() => {
         document.getElementById("intro").style.display = "none";
     }, 3000);
 
-    document.querySelector("form").addEventListener("submit", function (event) {
-        event.preventDefault();
-        alert("Το μήνυμα στάλθηκε επιτυχώς!");
-        this.reset();
+    // Μήνυμα επιβεβαίωσης για τις φόρμες
+    const forms = document.querySelectorAll("form");
+    forms.forEach(form => {
+        form.addEventListener("submit", function (event) {
+            event.preventDefault();
+            alert("Το μήνυμα στάλθηκε επιτυχώς!");
+            this.reset();
+        });
     });
 });
+Στάλθηκε
